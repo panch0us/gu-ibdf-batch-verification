00b3b15f27a1a2d8844cdfe8416d4c6beb0e74ec
savkin <godbryansk@yandex.ru>
2023-03-24 15:13:01 +0300

    теперь обрабатывает фио, состоящие из двух слов без тире

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ file_name = input('Введите название файла: ')
 
 lico = ''
 
+def split_line(lico, line):
+    if len(line.split()) == 1:
+        lico += ';'
+    if len(line.split()) == 2:
+        lico += line.split()[1] + ';'
+    if len(line.split()) == 3:
+        lico += line.split()[1] + line.split()[1] + ';'
+    return lico, line
+
 with open("provereno.txt", "w", encoding='windows-1251') as file_result:
     with open(file_name) as file:
         for line in file:
+            print(line)
             if 'Фамилия:' in line:
-                lico += line.split()[1] + ';'
+                lico, line = split_line(lico, line)
             if 'Имя:' in line:
-                lico += line.split()[1] + ';'
+                lico, line = split_line(lico, line)
             if 'Отчество:' in line:
-                lico += line.split()[1] + ';'
+                lico, line = split_line(lico, line)
             if 'Дата рождения:' in line:
                 lico += line.split()[2][6:] + ';;' + '\n'
+
     #print(lico)
     file_result.write(lico)
