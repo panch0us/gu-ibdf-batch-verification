9f357237a868cd9c4eab4cf8f3ceb47eb6abcd86
savkin <godbryansk@yandex.ru>
2024-03-05 12:03:04 +0300

    обработан вариант с отсутствием даты рождения

--- main.py
+++ main.py
@@ -19,12 +19,23 @@ with open("provereno.txt", "w", encoding='windows-1251') as file_result:
             print(line)
             if 'Фамилия:' in line:
                 lico, line = split_line(lico, line)
+                # Запоминаем фамилию для её отображения на случай ошибки
+                debug = line.split()[1]
             if 'Имя:' in line:
                 lico, line = split_line(lico, line)
             if 'Отчество:' in line:
                 lico, line = split_line(lico, line)
             if 'Дата рождения:' in line:
-                lico += line.split()[2][6:] + ';;' + '\n'
+                # Если 3 значения, значит 1 словло "Дата", 2 слово "рождения", 3  слово "**.**.****"
+                if len(line.split()) == 3:
+                    lico += line.split()[2][6:] + ';;' + '\n'
+                # Если 2 значения, значит 1 слово "Дата", 2 слово "рождения", а 3 слово - "дата рождения" не указано
+                if len(line.split()) == 2:
+                    lico += ';;' + '\n'
+                    # Обработка случая, когда не указана дата рождения
+                    with open('внимание.txt', 'w', encoding='windows-1251') as file_attension:
+                        result = f'У {debug} не указана дата рождения!'
+                        file_attension.write(result)
 
-    #print(lico)
+    print(lico)
     file_result.write(lico)
