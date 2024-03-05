ccfdb03a3e8f79dd059ff84bb449c5706a0ffdc6
savkin <godbryansk@yandex.ru>
2024-03-05 14:59:09 +0300

    обработка ошибки даты рождения через функцию

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+
+# Если файл есть в дирректории с программой - удаляем" (файл хранит ошибки от предыдущего запуска программы)
+try:
+    os.remove('внимание.txt')
+except OSError:
+    pass
+
 file_name = input('Введите название файла: ')
 
 lico = ''
@@ -13,6 +21,12 @@ def split_line(lico, line):
         lico += line.split()[1] + ' ' + line.split()[2] + ' ' + line.split()[3] + ';'
     return lico, line
 
+def attension(message: str) -> None:
+    """Возращает информацию в файле для пользователя об отсутствии Ф, И, О или Даты рождения"""
+    with open('внимание.txt', 'a', encoding='windows-1251') as file_attension:
+        file_attension.write(f'{debug} - {message}\n')
+
+
 with open("provereno.txt", "w", encoding='windows-1251') as file_result:
     with open(file_name) as file:
         for line in file:
@@ -26,16 +40,13 @@ with open("provereno.txt", "w", encoding='windows-1251') as file_result:
             if 'Отчество:' in line:
                 lico, line = split_line(lico, line)
             if 'Дата рождения:' in line:
-                # Если 3 значения, значит 1 словло "Дата", 2 слово "рождения", 3  слово "**.**.****"
-                if len(line.split()) == 3:
+                # Ожидаем, что первое слово "Дата", второе слово "рождения", третье  слово "ДД.ММ.ГГГГ"
+                # и что год рождения == 4 символам
+                if len(line.split()) == 3 and len(line.split()[2][6:]) == 4:
                     lico += line.split()[2][6:] + ';;' + '\n'
-                # Если 2 значения, значит 1 слово "Дата", 2 слово "рождения", а 3 слово - "дата рождения" не указано
-                if len(line.split()) == 2:
+                else:
                     lico += ';;' + '\n'
-                    # Обработка случая, когда не указана дата рождения
-                    with open('внимание.txt', 'w', encoding='windows-1251') as file_attension:
-                        result = f'У {debug} не указана дата рождения!'
-                        file_attension.write(result)
+                    attension('неверная дата рождения!\n')
 
-    print(lico)
+    #print(lico)
     file_result.write(lico)
